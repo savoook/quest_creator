d4fcd41d7a18bd10cd15cec147d2e55aba8a392c
Savin Pavel <psavin@ubic.tech>
2024-11-02 17:34:07 +0300

    init

---
+++ src/main/java/com/myquest/quest_creator/QuestCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.myquest.quest_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestCreatorApplication.class, args);
+	}
+
+}
